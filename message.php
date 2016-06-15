5e0a0aa4106796e9a3f2ae9a76b313660486ed2d
vincent.navales <vincent.navales@hotmail.fr>
2016-06-15 04:16:08 -0400

    exercice 1 fini

---
+++ message.php
@@ -0,0 +1,15 @@
+<?php
+extract($_POST);
+if(isset($_POST['langue'])){
+  $langue = $_POST['langue'];
+}
+if($langue == 1){
+  echo "Bonjour";
+}
+if($langue == 2){
+  echo "Hello";
+}
+if($langue == 3){
+  echo "Hola";
+}
+?>
